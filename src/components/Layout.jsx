65a311950326aab6662359b95be7b8e217e43eda
Madera <MaderaHLN@hotmail.com>
2022-05-31 15:46:42 -0500

    Componentes funcionando (parte inicial). Primera parte de Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+// import React from 'react';
+// import Header from './Header';
+// import Footer from './Footer';
+// import '../styles/components/Layout.css';
+
+// function Layout({ children }) {
+//   return (
+//     <div className="Main">
+//       <Header />
+//       {children}
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default Layout;
+
+import { Outlet } from 'react-router-dom'
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 import '../styles/components/Layout.css';
 
 function Layout({ children }) {
   return (
-    <div className="Main">
+    <div className='Main'>
       <Header />
-      {children}
+      <Outlet />
       <Footer />
     </div>
-  );
+  )
 }
 
-export default Layout;
+export default Layout
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
+// import React from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// import Home from '../containers/Home';
+// import Checkout from '../containers/Checkout';
+// import Information from '../containers/Information';
+// import Payment from '../containers/Payment';
+// import Success from '../containers/Success';
+// import NotFound from '../containers/NotFound';
+// import Layout from '../components/Layout';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Layout>
+//         <Routes>
+//           <Route exact path="/home" component={Home} />
+//           <Route exact path="/checkout" component={Checkout} />
+//           <Route exact path="/checkout/information" component={Information} />
+//           <Route exact path="/checkout/payment" component={Payment} />
+//           <Route exact path="/checkout/success" component={Success} />
+//           <Route component={NotFound} />
+//         </Routes>
+//       </Layout>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Information from '../containers/Information';
@@ -11,19 +40,19 @@ import Layout from '../components/Layout';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/checkout/information" component={Information} />
-          <Route exact path="/checkout/payment" component={Payment} />
-          <Route exact path="/checkout/success" component={Success} />
-          <Route component={NotFound} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='' element={<Home />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/checkout/information' element={<Information />} />
+          <Route path='/checkout/payment' element={<Payment />} />
+          <Route path='/checkout/success' element={<Success />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
